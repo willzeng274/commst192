f01b8bcc8fe1ff0490043ef011cf5f98456d3189
William Zeng <61915438+willzeng274@users.noreply.github.com>
2024-11-28 11:11:30 -0500

    deploy

--- src/lib/scenes/bobby.ts
+++ src/lib/scenes/bobby.ts
@@ -3,7 +3,7 @@ import type { Scene } from '../types';
 export const scenes: Record<number, Scene> = {
 	1: {
 		imgUrl: '/bob_s1.png',
-		text: 'The plane just crashed. (Plane in the background, luggage everywhere, idk picture the movie)',
+		text: 'The plane just crashed. Shattered luggage litters the snowy ground, survivors stumble in shock',
 		choices: [
 			{
 				imgUrl: '/bob_s1c1.png',
@@ -27,7 +27,7 @@ export const scenes: Record<number, Scene> = {
 	},
 	2: {
 		imgUrl: '/bob_s2.png',
-		text: 'The group asks Bobby to help search missing luggage containing critical supplies. (commander, he is walking in the direction opposite of the plane)',
+		text: 'The group asks Bobby to help search missing luggage containing critical supplies. He is walking around the plane aimlessly right now.',
 		choices: [
 			{
 				imgUrl: '/bob_s2c1.png',
@@ -41,7 +41,7 @@ export const scenes: Record<number, Scene> = {
 			{
 				imgUrl: '/bob_s2c2.png',
 				text: 'Bobby declines and stays behind.',
-				modal_text: 'Bobby dodges dying in the avalanche from the expedition, but the overall morale of the group is dropped. (Made up)',
+				modal_text: 'Bobby dodges dying in the avalanche from the expedition, but the overall morale of the group is dropped. This didn`t happen in reality, but it could have. (Made up)',
 				morale: -1,
 				health: 0,
 				survivor: 0,
@@ -51,7 +51,7 @@ export const scenes: Record<number, Scene> = {
 	},
 	3: {
 		imgUrl: '/bob_s3.png',
-		text: 'The group begins to resent Bobby for his selfish behaviour. The group is more stressed about his survival than himself.',
+		text: 'The group begins to resent Bobby for his selfish behaviour. The group is more stressed about his survival than himself. He is holding the group back in some ways, but in others he is sort of their puppy as they all have to take turns taking care for him.',
 		choices: [
 			{
 				imgUrl: '/bob_s3c1.png',
@@ -128,7 +128,7 @@ export const scenes: Record<number, Scene> = {
 	},
 	7: {
 		imgUrl: '/bob_s7.png',
-		text: 'Even though the group does eventually survive and Bobby is also rescued, his lack of participation endangered the group’s survival and caused a rift. He didn’t really contribute and he was primarily a waste of resources. ',
+		text: 'Even though the group does eventually survive and Bobby is also rescued, his lack of participation endangered the group’s survival and caused a rift. He didn’t really contribute and he was primarily a waste of resources.',
 		choices: null
 	},
 	8: {
@@ -147,7 +147,7 @@ export const scenes: Record<number, Scene> = {
 			{
 				imgUrl: '/bob_s8c2.png',
 				text: 'Bobby refuses to join the stays with the camp.',
-				modal_text: 'The group proceeds without Bobby. He stays behind and continues to hoard resources while others succeed in securing  rescue. (Happened in reality)  ',
+				modal_text: 'The group proceeds without Bobby. He stays behind and continues to hoard resources while others succeed in securing rescue. (Happened in reality)',
 				morale: 0,
 				health: 0,
 				survivor: 0,
@@ -156,17 +156,17 @@ export const scenes: Record<number, Scene> = {
 		]
 	},
 	9: {
-		imgUrl: '/bob_s9.png',
+		imgUrl: '/end_with_people.png',
 		text: 'The group’s cooperative efforts lead to the eventual rescue. Bobby’s willingness',
 		choices: null
 	},
 	10: {
-		imgUrl: '/bob_s10.png',
+		imgUrl: '/end_with_people.png',
 		text: 'Even though Bobby’s health has gotten worse, the expedition team successfully finds help. Bobby’s contribution is critical, earning him gratitude from the group. Note that in reality, they probably did not have enough food for 3 people to survive the expedition all the way the Chile since it barely lasted them in the real scenario.',
 		choices: null
 	},
 	11: {
-		imgUrl: '/bob_s11.png',
+		imgUrl: '/end_with_people.png',
 		text: 'The group is rescued due to the efforts of Nando and Canessa. Bobby survives, but his efforts were not as significant. However, he is still recognized as a contributor.',
 		choices: null
 	},
